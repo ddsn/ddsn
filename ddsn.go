5d102ea94b16a7584398eaee607ff3cfa9cd91a7
Julian Matschinske <julian@ddsn.org>
2014-12-17 15:29:10 +0100

    Add configuration via xml file

--- ddsn.go
+++ ddsn.go
@@ -2,8 +2,47 @@ package main
 
 import (
 	"fmt"
+	"flag"
+	"os"
+	"io/ioutil"
+
+	"encoding/xml"
 )
 
 func main() {
-	fmt.Println("Hello DDSN!")
+	fmt.Println("Welcome to DDSN 1.0")
+
+	// <Load configuration>
+
+	var configFile = flag.String("config", "config.xml", "file which contains configuration")
+	fmt.Println("Loading config information from " +(*configFile)+"...")
+
+	xmlFile, err := os.Open(*configFile)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+
+	b, _ := ioutil.ReadAll(xmlFile)
+
+	xmlFile.Close()
+
+	type server struct {
+		HttpPort  int
+		DdsnPort  int
+		TmlDir    string
+	}
+
+	type client struct {
+	}
+
+	type config struct {
+		Server  server
+		Client  client
+	}
+
+	var c config
+	xml.Unmarshal(b, &c)
+
+	// </Load configuration>
 }
